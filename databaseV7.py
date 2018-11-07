347f1df0faf393d5600df1a3ce1492ea4ceeacc8
DahMooingCow <cameronmcfie15@gmail.com>
2018-11-07 21:41:53 +1300

    Update

---
+++ databaseV7.py
@@ -0,0 +1,141 @@
+import sqlite3, time
+from tkinter import *
+from tkinter import ttk
+import tkinter as tk
+from tkinter import messagebox
+
+startTime = time.time()
+columnList = []
+testList = []
+conn = sqlite3.connect('data.db')
+
+cur = conn.cursor()
+
+
+# cur.execute("ALTER TABLE Games ADD COLUMN Completed")
+
+# def tableCreate():
+#     cur.execute("""CREATE TABLE Games(
+#                 id INTEGER PRIMARY KEY,
+#                 name TEXT,
+#                 score TEXT,
+#                 priority TEXT,
+#                 type TEXT,
+#                 genre TEXT,
+#                 multiplayer TEXT
+#                 )""")
+
+def addEntry():
+    addCount = int(input("How many Games do you want to add?"))
+    for i in range(0,addCount):
+        newName = input("What is the name of the new Game:")
+        newScore = input("What is the Score of the new Game[0-10]:")
+        newPriority = input("What is the priority of the new Game[1-5]:")
+        # newMulitplayer = input("What is the multiplayer of the new Game:")
+
+        cur.execute("""INSERT INTO Games(name, score, priority)
+                    VALUES(?,?,?)""", (newName, newScore, newPriority))
+        conn.commit()
+
+
+def columnCount():
+    cur.execute('''pragma table_info(Games);''')
+    numberOfColumns = len(cur.fetchall())
+    return numberOfColumns
+
+def columnNames():
+    cur.execute('''PRAGMA table_info(Games)''')
+    for col in cur.fetchall():
+        columnList.append(col[1])
+    return columnList
+
+def rowCount():
+    cur.execute('''SELECT * FROM Games''')
+    numberOfRows = len(cur.fetchall())
+    return numberOfRows
+
+
+cur.execute('''SELECT * FROM Games ORDER BY priority DESC;''')
+
+
+def update():
+    all_rows = cur.fetchall()
+    rowList = []
+    for row in all_rows:
+        rowList.append(row)
+        for item in row:
+            testList.append(item)
+
+
+def tree():
+    style = ttk.Style()
+    style.element_create("Custom.Treeheading.border", "from", "default")
+    style.layout("Custom.Treeview.Heading", [
+        ("Custom.Treeheading.cell", {'sticky': 'nswe'}),
+        ("Custom.Treeheading.border", {'sticky': 'nswe', 'children': [
+            ("Custom.Treeheading.padding", {'sticky': 'nswe', 'children': [
+                ("Custom.Treeheading.image", {'side': 'right', 'sticky': ''}),
+                ("Custom.Treeheading.text", {'sticky': 'we'})
+            ]})
+        ]}),
+    ])
+    style.configure("Custom.Treeview.Heading",
+                    background="#154360", foreground="white", relief="flat")
+    style.map("Custom.Treeview.Heading",
+              relief=[('active', 'groove'), ('pressed', 'sunken')])
+
+class App:
+    def __init__(self, master):
+        tree()
+        self.master = master
+        self.all_rows = cur.fetchall()
+        self.rowList = []
+        for self.row in self.all_rows:
+            self.rowList.append(self.row)
+        self.frame1, self.frame2 = Frame(self.master), Frame(self.master)
+        self.frame1.pack(side=TOP)
+        self.frame2.pack(side=BOTTOM)
+        self.but1 = Button(self.frame1, text='Left').pack(side=LEFT, anchor=N)
+        self.but2 = Button(self.frame1, text='This is the Center button').pack(side=LEFT, anchor=N)
+        self.but3 = Button(self.frame1, text='OK').pack(side=LEFT, anchor=N)
+        self.but4 = Button(self.frame2, text='Center').pack(side=TOP, fill=X, expand=YES)
+        columnNames()
+        self.tree = ttk.Treeview(self.frame2, columns=columnList, show='headings', height="400", style='Custom.Treeview')
+        self.tree.pack(side=BOTTOM)
+        self.tree.tag_configure('odd', background='#1A5276')
+        for col in columnNames():
+            self.tree.column(col, width=100)
+            self.tree.heading(col, text=col)
+        for index, row in enumerate(self.rowList):
+            self.tree.insert('', index, values=row, tags="odd")
+
+class Menu:
+    def __init__(self, master):
+        self.frame3 = master
+        Button(self.frame3, text="Database APP", command=self.create).pack()
+
+    def create(self):
+        top()
+
+def top():
+    menu.frame3.destroy()
+    top = Toplevel()
+    top.geometry("1000x800")
+    App(top)
+
+root = Tk()
+root.withdraw()
+root.option_add('*font', ('verdana', 12, 'bold'))
+root.title("Everything")
+menu = Menu(Toplevel())
+root.mainloop()
+conn.close()
+
+# endTime = time.time()
+# file = open("times.txt","a")
+# timeTaken = str(endTime-startTime)
+# info = "This script took "+timeTaken+" seconds"
+# file.write(timeTaken+"\n")
+# messagebox.showinfo("Info", info)
+# file.close()
+
